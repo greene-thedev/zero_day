395bd03a16c59b161f2267e410912e0dcf3519f8
Jeremiah Greene <jerrygreene010@gmail.com>
2023-08-05 14:48:45 +0000

    $commit_message

--- 0x00-vagrant/git_push.bat
+++ 0x00-vagrant/git_push.bat
@@ -1,13 +1,11 @@
-@REM #!/bin/bash
-
 echo "#change to your desired commit message"
-commit_message="Your commit message here"
+$commit_message = "Your commit message here"
 
 echo "# Add all files"
 git add .
 
 echo "# Commit with the specified message"
-git commit -m "$commit_message"
+git commit -m $commit_message
 
 echo "# Push changes to orign master"
 git push origin master
