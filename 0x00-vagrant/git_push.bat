39a4a130eb676cdf36b373fa02ee24bf7676a1c6
Jeremiah Greene <jerrygreene010@gmail.com>
2023-08-05 14:51:43 +0000

    new  message added

--- 0x00-vagrant/git_push.bat
+++ 0x00-vagrant/git_push.bat
@@ -1,11 +1,8 @@
-echo "#change to your desired commit message"
-$commit_message = "Your commit message here"
-
 echo "# Add all files"
 git add .
 
 echo "# Commit with the specified message"
-git commit -m $commit_message
+git commit -m "new  message added"
 
 echo "# Push changes to orign master"
 git push origin master
